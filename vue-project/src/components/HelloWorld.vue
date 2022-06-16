6b5108dd00d7c0c3fecbc77cca49df2f0041f8b8
James Bazan <jamesarmandobazan@gmail.com>
2022-06-16 15:10:48 -0500

    Start of Vue with Vuetify
    Start of Vue with Vuetify beta

---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout>
+    <v-app-bar color="grey-lighten-2"></v-app-bar>
+    <v-main>
+      <v-row>
+        <v-col
+          v-for="item in ecosystem"
+          :key="item"
+          xs="3"
+          sm="3"
+          md="3"
+          lg="3"
+        >
+          {{ item }}
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+
+  data() {
+    return {
+      ecosystem: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    };
+  },
+});
+</script>
---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <template>
+    <v-container fluid>
+      <v-textarea
+        name="input-7-1"
+        filled
+        label="Label"
+        auto-grow
+        value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+      ></v-textarea>
+    </v-container>
+  </template>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
